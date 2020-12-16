3550459d85cd41d57d2ed5de977d874b02771621
Lawrence Macharia <macharialawrence2011@gmail.com>
2020-12-16 03:17:19 +0300

    replaced the static list with the data from props

--- src/components/GoalList.js
+++ src/components/GoalList.js
@@ -1,11 +1,15 @@
 import React from 'react'
 
-const GoalList = () => {
+import './GoalList.css'
+
+const GoalList = props => {
   return (
     <ul className="goal-list">
-      <li>Finish the course</li>
-      <li>Learn all about the course main topic</li>
-      <li>Help other students in the course Q&amp;A</li>
+      {
+        props.goals.map(goal => {
+          return <li key={goal.id}>{goal.text}</li>
+        })
+      }
     </ul>
   )
 }
