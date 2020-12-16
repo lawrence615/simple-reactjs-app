ba62e074285cd22beddee1dd823697cd0d87f30e
Lawrence Macharia <macharialawrence2011@gmail.com>
2020-12-16 04:18:33 +0300

    two-way binding applied to beable to add our own goals

--- src/components/NewGoal/NewGoal.js
+++ src/components/NewGoal/NewGoal.js
@@ -1,8 +1,9 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 
 import './NewGoal.css'
 
 const NewGoal = props => {
+  const [enteredText, setEnteredText] = useState('')
 
   const addGoalHandler = event => {
     event.preventDefault();
@@ -12,11 +13,14 @@ const NewGoal = props => {
       text: 'Lorem Ipsum'
     }
 
+    setEnteredText('')
     props.onAddGoal(newGoal)
   }
 
+  const textChangeHandler = event => setEnteredText(event.target.value)
+
   return <form className="new-goal" onSubmit={addGoalHandler}>
-    <input type="text" />
+    <input type="text" value={enteredText} onChange={textChangeHandler} />
     <button type="submit">Add Goal</button>
   </form>
 }
